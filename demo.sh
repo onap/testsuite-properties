0724c91473d6b540cc9d3ddfecc69381167e206e
jf9860 <jf9860@att.com>
2017-02-24 17:15:57 -0500

    Add shells to support command line
    Change-Id: I8c449174b7adafed3138befabb1b777b7e86e9e9
    Signed-off-by: jf9860 <jf9860@att.com>

---
+++ demo.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#
+# Execute tags built to support the hands on demo,
+# init - Distributes models demoVFW and demoVLB, and creates customer "Demonstration"
+# preload - Preloads the VNF <vnf_name> that was provided in the VID interface and
+#           preloads the <module_name> that will be provided to VID when instantiating the VNF
+#
+# appc - Will configure APPC for the VFW <module_name> for a closed loop test
+#
+
+# Set the defaults
+if [ $# -eq 0 ];then
+	echo "Usage: demo.sh init"
+	echo "       demo.sh preload <vnf_name> <module_name>"
+	echo "       demo.sh appc <module_name>"
+	exit
+fi
+##
+## if more than 1 tag is supplied, the must be provided with -i or -e
+##
+while [ $# -gt 0 ]
+do
+	key="$1"
+
+	case $key in
+    	init)
+    	TAG="InitDemo"
+    	shift
+    	;;
+    	preload)
+    	TAG="PreloadDemo"
+    	shift
+    	if [ $# -ne 2 ];then
+			echo "Usage: demo.sh preload <vnf_name> <module_name>"
+			exit
+		fi
+    	VARIABLES="$VARIABLES -v VNF_NAME:$1"
+    	shift
+    	VARIABLES="$VARIABLES -v MODULE_NAME:$1"
+    	shift
+    	;;
+    	appc)
+    	TAG="APPCMountPointDemo"
+    	shift
+    	if [ $# -ne 1 ];then
+			echo "Usage: demo.sh appc <module_name>"
+			exit
+		fi
+    	VARIABLES="$VARIABLES -v MODULE_NAME:$1"
+    	shift
+    	;;
+    	*)
+    	echo "Usage: demo.sh init"
+    	echo "       demo.sh preload <vnf_name> <module_name>"
+   	    echo "       demo.sh appc <module_name>"
+    	exit
+	esac
+done
+
+ETEHOME=/var/opt/OpenECOMP_ETE
+VARIABLEFILES="-V /share/config/vm_properties.py -V /share/config/robot_properties_ete.py -V /share/config/robot_preload_parameters.py"
+docker exec openecompete_container ${ETEHOME}/runTags.sh ${VARIABLEFILES} ${VARIABLES} -d /share/logs/demo/${TAG} -i ${TAG} --display 89 2> ${TAG}.out
