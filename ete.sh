433d4dd069d590522e1cd95bd98074aa06c5cd3f
Jerry Flood <jf9860@att.com>
2017-03-22 16:07:20 -0400

    Fix ete.sh documentation and usage
    Change-Id: I7027a09e19306bb3987de2526bb805586e63b778
    Signed-off-by: Jerry Flood <jf9860@att.com>

--- ete.sh
+++ ete.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 #
-# Run the testsuite for the passed tag. Valid tags are ete, healthcheck, closedloop, orchestration
+# Run the testsuite for the passed tag. Valid tags are ete, health, closedloop, orchestration
 # Please clean up logs when you are done...
 # Note: Do not run multiple concurrent ete.sh as the --display is not parameterized and tests will collide
 #
+if [ "$1" == "" ];then
+   echo "Usage: ete.sh [ health | ete | closedloop | orhcestration ]"
+   exit
+fi
 
-TAGS=-i $1
+export TAGS="-i $1"
 export ETEHOME=/var/opt/OpenECOMP_ETE
 export OUTPUT_FOLDER=ETE_$$
 
 VARIABLEFILES="-V /share/config/vm_properties.py -V /share/config/integration_robot_properties.py -V /share/config/integration_preload_parameters.py"
 VARIABLES="-v GLOBAL_BUILD_NUMBER:$$"
 
-docker exec openecompete_container ${ETEHOME}/runTags.sh ${VARIABLEFILES} ${VARIABLES} -d /share/logs/${OUTPUT_FOLDER} ${TAGS} –display 88
+docker exec openecompete_container ${ETEHOME}/runTags.sh ${VARIABLEFILES} ${VARIABLES} -d /share/logs/${OUTPUT_FOLDER} ${TAGS} --display 88
