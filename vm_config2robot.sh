d91dec7ea498190394e436c5b86ce0ebeea1d4b8
Jerry Flood <jf9860@att.com>
2017-10-31 08:03:30 -0400

    Create dictionary of all GLOBAL_INJECTED
    Include all GLOBAL_INJECTED variables in a dicitonary as well
    to make available for template processing.
    
    Issue: TEST-70
    Change-Id: Icfdccfd36c946c576c45d5b1c452660411fe424c
    Signed-off-by: Jerry Flood <jf9860@att.com>

--- vm_config2robot.sh
+++ vm_config2robot.sh
@@ -6,6 +6,8 @@
 CONFIG=/opt/config
 PROPERTIES=/opt/eteshare/config/vm_properties.py
 GLOBAL_VM_PROPERTIES="# File generated from /opt/config\n#\n"
+HASH="GLOBAL_INJECTED_PROPERTIES={"
+COMMA+""
 for f in `ls $CONFIG/*.txt`;
 do
     VALUE=`cat $f`
@@ -13,6 +15,10 @@ do
     NAME=${NAME##*/}
     NAME=${NAME^^}
     GLOBAL_VM_PROPERTIES=$"${GLOBAL_VM_PROPERTIES}GLOBAL_INJECTED_$NAME = \"$VALUE\"\n"
+	HASH=$"${HASH}${COMMA}\n\"GLOBAL_INJECTED_$NAME\" : \"$VALUE\""
+	COMMA=","
 done
+HASH="${HASH}}\n"
+GLOBAL_VM_PROPERTIES="${GLOBAL_VM_PROPERTIES}\n${HASH}"
 GLOBAL_VM_PROPERTIES=${GLOBAL_VM_PROPERTIES}
 echo -e $GLOBAL_VM_PROPERTIES > $PROPERTIES
