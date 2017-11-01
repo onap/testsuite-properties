a60a493d29f665caf3154942a02e7f85839fcd4b
Helen Chen <Y00725961@MacBook-Pro-3.local>
2017-11-01 00:08:07 -0700

    Fixed dcae health check
    It will pass when VES, HOMLES, etc. be added.
    
    Change-Id: If33e369700aa5d0b9b40bc1654075e12db07a71d
    Issue-ID: DCAEGEN2-186
    Signed-off-by: Helen Chen <helen.chen@huawei.com>

--- integration_robot_properties.py
+++ integration_robot_properties.py
@@ -24,7 +24,7 @@ GLOBAL_CLAMP_SERVER_PROTOCOL = "http"
 GLOBAL_CLAMP_SERVER_PORT = "8080"
 # dcae info - everything is from the private oam network (also called ecomp private network)
 GLOBAL_DCAE_SERVER_PROTOCOL = "http"
-GLOBAL_DCAE_SERVER_PORT = "9998"
+GLOBAL_DCAE_SERVER_PORT = "8080"
 GLOBAL_DCAE_USERNAME = "console"
 GLOBAL_DCAE_PASSWORD = "ZjJkYjllMjljMTI2M2Iz"
 # microservice bus info - everything is from the private oam network (also called ecomp private network)
