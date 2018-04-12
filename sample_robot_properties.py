d87e2498c41fe81c5dd8184babcb3e70aa219369
DR695H <dr695h@att.com>
2018-04-11 22:22:13 -0400

    long overdue property cleanup
    Add in correct comments and adding in new properties to sample
    
    Change-Id: Ia4b1d6b8bd180ec9b95bafef1d095543e918502e
    Issue-ID: TEST-87
    Signed-off-by: DR695H <dr695h@att.com>

--- sample_robot_properties.py
+++ sample_robot_properties.py
@@ -1,43 +1,54 @@
-# aaf info - everything is from the private oam network (also called ecomp private network)
+# aaf info - everything is from the private oam network (also called onap private network)
 GLOBAL_AAF_SERVER = "http://10.0.12.1:8101"
 GLOBAL_AAF_USERNAME = "username"
 GLOBAL_AAF_PASSWORD = "password"
-# aai info - everything is from the private oam network (also called ecomp private network)
+# aai info - everything is from the private oam network (also called onap private network)
 GLOBAL_AAI_SERVER_PROTOCOL = "https"
 GLOBAL_AAI_SERVER_PORT = "8443"
 GLOBAL_AAI_USERNAME = "username"
 GLOBAL_AAI_PASSWORD = "password"
-# appc info - everything is from the private oam network (also called ecomp private network)
+# appc info - everything is from the private oam network (also called onap private network)
 GLOBAL_APPC_SERVER_PROTOCOL = "http"
 GLOBAL_APPC_SERVER_PORT = "8282"
 GLOBAL_APPC_USERNAME = "username"
 GLOBAL_APPC_PASSWORD = "password"
-# sdc info - everything is from the private oam network (also called ecomp private network)
+# sdc info - everything is from the private oam network (also called onap private network)
 GLOBAL_ASDC_SERVER_PROTOCOL = "http"
 GLOBAL_ASDC_FE_PORT = "8181"
 GLOBAL_ASDC_BE_PORT = "8080"
 GLOBAL_ASDC_BE_USERNAME = "username"
 GLOBAL_ASDC_BE_PASSWORD = "password"
-# dcae info - everything is from the private oam network (also called ecomp private network)
+# clamp info - everything is from the private oam network (also called onap private network)
+GLOBAL_CLAMP_SERVER_PROTOCOL = "http"
+GLOBAL_CLAMP_SERVER_PORT = "8080"
+# dcae info - everything is from the private oam network (also called onap private network)
 GLOBAL_DCAE_SERVER_PROTOCOL = "http"
-GLOBAL_DCAE_SERVER_PORT = "9998"
+GLOBAL_DCAE_SERVER_PORT = "8080"
 GLOBAL_DCAE_USERNAME = "username"
 GLOBAL_DCAE_PASSWORD = "password"
-# message router info - everything is from the private oam network (also called ecomp private network)
+# microservice bus info - everything is from the private oam network (also called onap private network)
+GLOBAL_MSB_SERVER_PROTOCOL = "http"
+GLOBAL_MSB_SERVER_PORT = "80"
+# message router info - everything is from the private oam network (also called onap private network)
 GLOBAL_MR_SERVER_PROTOCOL = "http"
 GLOBAL_MR_SERVER_PORT = "3904"
-# mso info - everything is from the private oam network (also called ecomp private network)
+# mso info - everything is from the private oam network (also called onap private network)
 GLOBAL_MSO_SERVER_PROTOCOL = "http"
 GLOBAL_MSO_SERVER_PORT = "8080"
 GLOBAL_MSO_USERNAME = "username"
 GLOBAL_MSO_PASSWORD = "password"
+# music info - everything is from the private oam network (also called onap private network)
+GLOBAL_MUSIC_SERVER_PROTOCOL = "http"
+GLOBAL_MUSIC_SERVER_PORT = "8080"
+GLOBAL_MUSIC_USERNAME = "username"
+GLOBAL_MUSIC_PASSWORD = "password"
 # openstack info - info to select right info in environment
-# packet generate vnf info - everything is from the private oam network (also called ecomp private network)
+# packet generate vnf info - everything is from the private oam network (also called onap private network)
 GLOBAL_PACKET_GENERATOR_PORT = "8183"
 GLOBAL_PACKET_GENERATOR_USERNAME = "username"
 GLOBAL_PACKET_GENERATOR_PASSWORD = "password"
 GLOBAL_PGN_PORT = "2831"
-# policy info - everything is from the private oam network (also called ecomp private network)
+# policy info - everything is from the private oam network (also called onap private network)
 GLOBAL_POLICY_SERVER_PROTOCOL = "http"
 GLOBAL_POLICY_SERVER_PORT = "8081"
 GLOBAL_POLICY_HEALTHCHECK_PORT = "6969"
@@ -45,17 +56,20 @@ GLOBAL_POLICY_AUTH = "auth"
 GLOBAL_POLICY_CLIENTAUTH = "clientauth="
 GLOBAL_POLICY_USERNAME = "username"
 GLOBAL_POLICY_PASSWORD = "password"
-# portal info - everything is from the private oam network (also called ecomp private network)
-GLOBAL_PORTAL_SERVER = "http://10.0.9.1:8989"
+# portal info - everything is from the private oam network (also called onap private network)
+GLOBAL_PORTAL_SERVER_PROTOCOL = "http"
+GLOBAL_PORTAL_SERVER_PORT = "8989"
 GLOBAL_PORTAL_USERNAME = "username"
 GLOBAL_PORTAL_PASSWORD = "password"
-# sdngc info - everything is from the private oam network (also called ecomp private network)
+# sdngc info - everything is from the private oam network (also called onap private network)
 GLOBAL_SDNGC_SERVER_PROTOCOL = "http"
 GLOBAL_SDNGC_REST_PORT = "8282"
 GLOBAL_SDNGC_ADMIN_PORT = "8843"
 GLOBAL_SDNGC_USERNAME = "username"
 GLOBAL_SDNGC_PASSWORD = "password"
-# vid info - everything is from the private oam network (also called ecomp private network)
+# vid info - everything is from the private oam network (also called onap private network)
+GLOBAL_VID_SERVER_PROTOCOL = "http"
+GLOBAL_VID_SERVER_PORT = "8080"
 GLOBAL_VID_USERNAME = "username"
 GLOBAL_VID_PASSWORD = "password"
 GLOBAL_VID_HEALTH_USERNAME = "username"
@@ -63,8 +77,7 @@ GLOBAL_VID_HEALTH_PASSWORD = "password"
 #global selenium info
 GLOBAL_PROXY_WARNING_TITLE=""
 GLOBAL_PROXY_WARNING_CONTINUE_XPATH=""
-# settings for vm to attach vvg too
-GLOBAL_VVGSERVER_IMAGE = "Ubuntu 14.04 LTS (Trusty Tahr) (PVHVM)"
-GLOBAL_VVGSERVER_FLAVOR = "4 GB General Purpose v1"
 # dns info
 GLOBAL_DNS_TRAFFIC_DURATION = "600"
+# location where heat templates are loaded from
+GLOBAL_HEAT_TEMPLATES_FOLDER = "/share/heat"
